c1b63259dacc690fff8749d2d0c71de9a075b06b
Darren White <github@darrenwhite.org>
2019-12-25 17:03:22 -0800

    Added Channel Sorting
    Channels are assigned to groups, groups are sorted and channels are sorted within groups.

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
+	function chcompare(a, b) {
+		const preferredorder = 'Recommended,Vendors,Other'.split(','); // Specified groups will be sorted in this order. Unmatched groups will be sorted alphabetically
+		var groupA = preferredorder.indexOf(a.chgroup)+1;
+		var groupB = preferredorder.indexOf(b.chgroup)+1;
+		groupA = (groupA > 0) ? groupA : (preferredorder.length + 1);
+		groupB = (groupB > 0) ? groupB : (preferredorder.length + 1);
+
+		return (
+			(groupA === groupB) ? ( (a.chgroup.localeCompare(b.chgroup) === 0) ? a.name.localeCompare(b.name) : a.chgroup.localeCompare(b.chgroup) ) : (groupA - groupB)
+		); // Groups are sorted as stated above Channels within the same group are sorted alphabetically
+	}
+
 	function loadChannelList(results) { //pulls the list of channels from the JSON response and creates checkboxes
 		$("#hiddenJSON")[0].innerHTML = results
-			var a = JSON.parse(results);
-			var cbh = $("#cb");
-			var html = '';
-			for (let i = 0, l = a.length;i<l;i++) {
-				if (!a[i].is_archived) {
+		var a = JSON.parse(results);
+		var cbh = $("#cb");
+		var html = '';
+		for (let i = 0, l = a.length;i<l;i++) { // Assigns a channel group property to each channel
+			if (/^(announcements|cw-automate|geekcast|msp-general|rmm-general)$/i.test(a[i].name)) {
+				a[i].chgroup = 'Recommended';
+			} else if (/^(v|datto)-.+/i.test(a[i].name)) {
+				a[i].chgroup = 'Vendors';
+			} else if (/^(random|games|stargate)$/i.test(a[i].name)) {
+				a[i].chgroup = 'For Fun';
+			} else {
+				a[i].chgroup = 'Other';
+			}
+		}
+		a.sort(chcompare);
+		for (let i = 0, l = a.length;i<l;i++) {
+			if (!a[i].is_archived) {
 				//var start = i % 2 === 0 ? '<p>' : '';
 				//var end = i % 2 !== 0 ? '</p>' : '';
 				var htmlString = '<label for="' + a[i].id + '" class="form-check-label" style="padding-right: 25px;"><input type="checkbox" value="' + a[i].name + '" id="' + a[i].id + '" class="form-check" />' + a[i].name + '</label>';
-				html += htmlString;}
+				html += htmlString;
 			}
+		}
 		cbh.html(html);
 	}
 
